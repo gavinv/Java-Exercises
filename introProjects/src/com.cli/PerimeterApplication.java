40fec771573bc04d4be2d81c08f082b1f24057bc
Gavin <gavinvaught@gmail.com>
2016-09-23 10:58:19 -0500

    started working on lab projects. first command line app for perim and area of room

---
+++ introProjects/src/com.cli/PerimeterApplication.java
@@ -0,0 +1,26 @@
+package com.cli; /**
+ * Created by gavin on 9/22/16.
+ */
+import java.util.Scanner;
+public class PerimeterApplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Room Detail Generator!");
+        String inputYes = "y";
+
+        while (inputYes.equalsIgnoreCase("y") || inputYes.equalsIgnoreCase("yes")) {
+            System.out.println("Enter length in feet:");
+            double length = 0;
+            System.out.println("Enter the width in feet:");
+            double width = 0;
+
+            double area = width * length;
+            double perimeter = (2 * width) + (2 * length);
+            System.out.println("Area: " + area);
+            System.out.println("Perimeter: " + perimeter);
+            System.out.println("Continue? (y/n):");
+            inputYes = sc.next();
+        }
+        System.out.println("Thank you. Goodbye...");
+    }
+}
